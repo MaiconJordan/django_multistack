2664ed9e6e654c8c7e67e08cff51ee479a800e7b
MaiconJordan <maiconplus@gmail.com>
2024-10-13 20:26:08 -0300

    migrations

--- django_multistack/app/models.py
+++ django_multistack/app/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models # type: ignore
 
 # Create your models here.
+class Tarefa(models.Model):
+    STATUS_CHOICE = [
+        (1, 'A fazer'),
+        (2, 'Fazendo'),
+        (3, 'Concluido')
+    ]
+
+    titulo = models.CharField(max_length=100, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    data = models.DateField(null=False, blank=False)
+    status = models.IntegerField(choices=STATUS_CHOICE, null=False, blank=False)
+
